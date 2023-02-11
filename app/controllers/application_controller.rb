cc0a5c5ad938b68a2c09e7e7de6e20c666961bdf
Shubh <shubh.scb@gmail.com>
2023-02-11 14:07:22 +0530

    LINTERS ERR FIX

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
-    before_action :update_allowed_parameters, if:
-    :devise_controller?
+  before_action :update_allowed_parameters, if:
+  :devise_controller?
 
-    protected
+  protected
 
-    def update_allowed_parameters
-        devise_parameter_sanitizer.permit(:sign_up) { |u|
-        u.permit(:first_name, :last_name, :email, :password, :current_password)}
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:first_name, :last_name, :email, :password, :current_password)
     end
+  end
 end
