eafcf93e979df8cf420486a1e4f1afa25096c73f
Shubh <shubh.scb@gmail.com>
2023-02-12 23:31:59 +0530

    REDUX STORE IMPLEMENT COMPLETE

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-const job = () = {
-    
-}
+const App = () => {
+    const jobs = useSelector(state => state);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <p>{jobs}</p>
+        </div>
+    )
+};
+
+export default App;
 
 
 
