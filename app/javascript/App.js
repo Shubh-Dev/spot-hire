5227ab9d8d230dcc51a0846a510f1f61e1cbc763
Shubh <shubh.scb@gmail.com>
2023-02-17 23:44:37 +0530

    ?

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { useSelector } from "react-redux";
+// import { useDispatch } from "react-redux";
 
-const App = () => (
-    <h1>Hello hire Spot!</h1>
-);
+
+const App = () => {
+    // const dispatch = useDispatch();
+    const jobs = useSelector((state) => state.jobs);
+    console.log(jobs)
+
+    return (
+        <div>
+            <h1>Jobs</h1>
+            <ul>
+                {jobs.map((job) => (
+                    <li key={job.id}>
+                        <h2>{job.title}</h2>
+                        <p>{job.company}</p>
+                        <p>{job.description}</p>
+                        <p>{job.employees}</p>
+                        <p>{job.skills}</p>
+                        <p>{job.salary}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};  
 
 export default App;
